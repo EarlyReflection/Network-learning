78638c2a7bf042c5995a3a6558ab443e52e712c8
Vladimir Dvornikov <echobizarre@gmail.com>
2022-11-03 22:36:39 +0200

    added GET request and JSON

--- Network learning/ButtonsViewController.swift
+++ Network learning/ButtonsViewController.swift
@@ -8,8 +8,30 @@
 import UIKit
 
 class ButtonsViewController: UIViewController {
-
+    
     @IBAction func getRequest(_ sender: Any) {
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        let session = URLSession.shared
+        session.dataTask(with: url) { data, responce, error in
+            
+            guard
+                let responce = responce,
+                let data = data
+            else { return }
+            
+            print(responce)
+            print(data)
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            } catch {
+                print(error)
+            }
+            
+        }.resume()
+        
     }
     
     @IBAction func postRequest(_ sender: Any) {
